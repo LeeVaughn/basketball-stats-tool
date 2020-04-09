e7587ca45e2bf660fb64eac2de1b08618a5c3384
LeeVaughn <dlvprogramming@gmail.com>
2020-04-09 09:36:56 -0700

    planned out objectives for the project using comments

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
+# import player data
+
+# clean data
+    # create separate Lists for TEAMS and PlAYERS?
+    # convert height string to int
+    # convert experienced string to boolean
+    # split guardian field into a List of strings
+
+# balance the number of players equally between teams
+    # teams should have an equal number of experienced and inexperienced players
+        # might need to split experienced and inexperienced players into separate Lists for this?
+
+# display stats
+    # team name
+    # number of players on team
+    # number of experience/inexperienced players (maybe combine these two steps?)
+    # average height of the team
+    # player names seperated by commas
+    # guardians of all players seperated by commas
+
+# create a menu for user interaction
+    # user should be able to display a given teams stats
+    # user should be able to quit
+    # user should be reprompted with the main menu until they quit the program
+
 if __name__ == "__main__":
     # run initial function
