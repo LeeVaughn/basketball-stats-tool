fa01fc8b1498c142b6d223b8c9bf9bfc90b11e06
LeeVaughn <dlvprogramming@gmail.com>
2020-04-13 19:25:47 -0700

    display stats function showing team name, number of players, and avg height

--- app.py
+++ app.py
@@ -31,13 +31,14 @@ def draft(teams_list, players_list):
     global panthers, bandits, warriors
     experienced = []
     inexperienced = []
-    panthers = [{
-        "team": TEAMS[0],
-        "num_players": 0,
-        "num_exp": 0,
-        "num_inexp": 0,
-        "avg_height": 0
-    }]
+    # panthers = [{
+    #     "team": TEAMS[0],
+    #     "num_players": 0,
+    #     "num_exp": 0,
+    #     "num_inexp": 0,
+    #     "avg_height": 0
+    # }]
+    panthers = []
     bandits = []
     warriors = []
 
@@ -70,13 +71,28 @@ def draft(teams_list, players_list):
 
 
 # display stats
-def display_stats(team, stats):
+def display_stats(team, players):
+    exp_players = 0
+    inexp_players = 0
+    height = 0
+
+    # calculate stats
+    for player in players:
+        if player["experience"] == True:
+            exp_players += 1
+        elif player["experience"] == False:
+            inexp_players += 1
+
+        height += player["height"]
+
     # team name
-    print("Team Name: ", team)
+    print("Displaying stats for the {}.".format(team))
     # number of players on team
-    print("There are {} players on the team".format(len(stats)))
+    print("There are {} players on the team,".format(len(players)))
     # number of experience/inexperienced players (maybe combine these two steps?)
+    print("{} experienced players and {} inexperienced.".format(exp_players, inexp_players))
     # average height of the team
+    print("The average player height is {} inches.".format(height / len(players)))
     # player names seperated by commas
     # guardians of all players seperated by commas
 
@@ -92,6 +108,7 @@ def menu(teams_list, players_list):
     # print(rosters)
     # user should be able to display a given teams stats
     display_stats(TEAMS[0], panthers)
+    print(panthers)
     # user should be able to quit
     # user should be reprompted with the main menu until they quit the program
 
